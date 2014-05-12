f9d7e0a728a04a7bb3d50d879621cb080747437d
Dariusz Pawlak <d.pawlak@wurth.pl>
2014-05-12 15:01:47 +0200

    add index.html

--- app.js
+++ app.js
@@ -10,9 +10,13 @@ Dariusz Pawlak <pawlakdp@gmail.com>
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/static'));
+
 app.get('/', function(req, res){
-  res.send('Express 4 + Angular + Socket.io minimal example application');
+  res.sendfile('index.html');
 });
+
+
 
 app.listen(3000);
 
